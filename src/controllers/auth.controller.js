c1d8ec752a067d125eab22f8dce8acdc879fe549
Nitin <nitinsharmaop828@gmail.com>
2026-04-27 17:21:04 +0530

    cookie update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -123,7 +123,7 @@ res.cookie("refreshToken",refreshToken,{
             httpOnly: true,
             secure: true,
             sameSite: "strict",
-            maxAge: 7 *24 * 60 * 60 * 100
+            maxAge: 7 *24 * 60 * 60 * 1000
         })
         
 
