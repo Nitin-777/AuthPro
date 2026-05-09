2461093af9fad93cbf5b8252d4746d00cee802c4
Nitin <nitinsharmaop828@gmail.com>
2026-05-09 17:02:12 +0530

    config.js

--- src/config/config.js
+++ src/config/config.js
@@ -8,11 +8,28 @@ if(!process.env.MONGO_URI){
 if(!process.env.JWT_SECRET){
      throw new error("JWT_SECRET is not defined in env")
 }
+if(!process.env.GOOGLE_CLIENT_ID){
+     throw new error("JWT_SECRET is not defined in env")
+}
+if(!process.env.GOOGLE_CLIENT_SECRET){
+     throw new error("JWT_SECRET is not defined in env")
+}
+if(!process.env.GOOGLE_USER){
+     throw new error("JWT_SECRET is not defined in env")
+}
+if(!process.env.GOOGLE_REFRESH_TOKEN){
+     throw new error("JWT_SECRET is not defined in env")
+}
 
 
 const config={
      MONGO_URI: process.env.MONGO_URI,
      JWT_SECRET: process.env.JWT_SECRET,
+     GOOGLE_REFRESH_TOKEN:process.env.GOOGLE_REFRESH_TOKEN,
+     GOOGLE_USER:process.env.GOOGLE_USER,
+     GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET,
+     GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
+
 }
 
 export default config;
